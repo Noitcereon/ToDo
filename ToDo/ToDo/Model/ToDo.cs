7fdad82a5a463fa0843a1f5180fc1a4ee296a08d
Ronnie Damsgaard <Ronnie1983@me.com>
2020-03-03 10:55:10 +0100

    Created ToDo Class

---
+++ ToDo/ToDo/Model/ToDo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Model
+{
+    class ToDo
+    {
+        #region Instans felter
+        private string _task;
+        private DateTime _dato;
+        private int _id; 
+        #endregion
+
+        #region Properties
+
+        public string Task
+        {
+            get => _task;
+            set => _task = value;
+        }
+
+        public DateTime Dato
+        {
+            get => _dato;
+            set => _dato = value;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        #endregion
+
+
+        #region Constructor
+        public ToDo()
+        {
+            _task = "Empty";
+            _dato = DateTime.Now;
+        }
+
+        public ToDo(string task, DateTime dato, int id)
+        {
+            _task = task;
+            _dato = dato;
+            _id = id;
+        }
+        #endregion
+
+
+    }
+}
