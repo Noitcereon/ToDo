4a4698d111d8586e1caaf09ef99bf91526dcb207
Noitcereon <thomaswowundercover@gmail.com>
2020-03-10 10:17:10 +0100

    Added connectionstring

---
+++ ToDo/ToDo/DbUtility/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DbUtility
+{
+    class Manager
+    {
+        private const string ConnectionString = "Data Source=serverdamsgaard.database.windows.net;" +
+                                                "Initial Catalog=ToDoDB;User ID=zealandadmin;Password=********;" +
+                                                "Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;" +
+                                                "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
